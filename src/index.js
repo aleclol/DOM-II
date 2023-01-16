148383102cf42972ea197b28bec09c2ec259bfc5
Alec Barber <alecbarber23@gmail.com>
2023-01-16 13:19:01 -0500

    added two bits of functionality

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ heading1.addEventListener('click', (event) => {event.target.style.color = `rgb($
 
 // Highlight nav bar items
 const navHover = document.querySelector('nav');
-navHover.addEventListener('mouseover', (event) => {event.target.style.color = "blue";
+navHover.addEventListener('mouseover', (event) => {
+    event.target.style.color = "blue";
+    //event.target.style.backgroundColor = "blue";
 })
 navHover.addEventListener('mouseout', (event) => {event.target.style.color = "";
 })
@@ -19,4 +21,21 @@ document.addEventListener('copy', (event) => {
     setTimeout(() => {event.target.textContent = origText;}, 1000);
 });
 
-//heading1.addEventListener('dblclick', (event) => {event.target.style.animation = '3s infinite alternate slidein;'})
+
+// idk DNW
+const navLinkHover = document.querySelectorAll('nav a');
+//navLinkHover.children.addEventListener('mouseover', (event)=> event.target.style.backgroundColor = 'gray')
+
+// select DNW
+document.body.addEventListener('select', (event)=> event.target.style.colors = 'gray')
+
+
+// Prevent spacebar from scrolling you down the page
+document.addEventListener('keypress', (event) => {
+    if(event.key === " "){event.preventDefault();}
+    }  
+);
+
+// adding text to the first paragraph just by typing
+const introParagraph = document.querySelector('.intro p');
+document.addEventListener('keypress', (event) => introParagraph.textContent += event.key);
